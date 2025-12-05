60fe3ca4ac2efd862e033ca0a7da5ff5be25e9ae
girl0330 <girls0330@naver.com>
2025-12-06 00:13:21 +0900

    feat: 사이드 버튼 아이콘, onClick() prop 추가

--- src/app/guide/page.tsx
+++ src/app/guide/page.tsx
@@ -1,14 +1,28 @@
+"use client";
+
 import ModalButton from "@/components/common/ModalButton";
 import SideButton from "@/components/common/SideButton";
+import Image from "next/image";
 import React from "react";
 
 const page = () => {
   return (
     <div className="flex">
       <aside className="w-[64px] md:w-[192px] min-h-screen bg-white border-r ">
-        <SideButton>사이드 버튼1</SideButton>
-        <SideButton>사이드 버튼2</SideButton>
-        <SideButton>사이드 버튼3</SideButton>
+        <SideButton
+          icon={<Image src="active.svg" width={24} height={24} alt="home icon" />}
+          onClick={() => {
+            console.log("클릭함!");
+          }}
+        >
+          사이드 버튼1
+        </SideButton>
+        <SideButton icon={<Image src="active.svg" width={24} height={24} alt="home icon" />}>
+          사이드 버튼2
+        </SideButton>
+        <SideButton icon={<Image src="active.svg" width={24} height={24} alt="home icon" />}>
+          사이드 버튼3
+        </SideButton>
       </aside>
       <ModalButton>모달 버튼</ModalButton>
     </div>
--- src/components/common/SideButton.tsx
+++ src/components/common/SideButton.tsx
@@ -1,14 +1,21 @@
+"use client";
+
 import React from "react";
 
 type SideButtonProps = {
+  icon?: React.ReactNode;
   children: React.ReactNode;
+  onClick?: () => void;
 };
 
-const SideButton: React.FC<SideButtonProps> = ({ children }) => {
+const SideButton: React.FC<SideButtonProps> = ({ icon, children, onClick }) => {
   return (
     <div>
-      <button className="flex items-center inline-block border border-gray-300 w-full md:w-full h-[48px] px-5 ">
-        <div className="w-[24px] h-[24px] bg-gray-100 rounded-md"></div>
+      <button
+        className="flex items-center inline-block border border-gray-300 w-full md:w-full h-[48px] px-5 "
+        onClick={onClick}
+      >
+        <div>{icon}</div>
         <span className="text-[16px] text-center ml-0 md:ml-[8px] hidden md:inline">
           {children}
         </span>
